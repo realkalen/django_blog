a000adb0ef1d4ef486c87239757195432298746d
realkalen <bettevans@yahoo.com>
2023-02-19 13:49:00 +0300

    update in admin site

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-
-    },
-    {
-       'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018' 
-    }
-]
 
 def home(request):
     context = {
