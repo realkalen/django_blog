3c60cd71a617aaf0c3ff1f2cdc3300099e943035
realkalen <bettevans@yahoo.com>
2023-02-23 00:18:35 +0300

    Profile redirect

--- django_project/settings.py
+++ django_project/settings.py
@@ -125,6 +125,7 @@ STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
