f0609b4a9cfe9e685b785a772cf058128ee6d8cf
realkalen <bettevans@yahoo.com>
2023-02-28 23:48:47 +0300

    change post order

--- blog/views.py
+++ blog/views.py
@@ -14,6 +14,7 @@ class PostListView(ListView):
       model = Post
       template_name = 'blog/home.html'
       context_object_name = 'posts'
+      ordering = '-date_posted'
 
 def about(request):
         return render(request, 'blog/about.html', {'title': 'About'})
