4ae69b130cc7237c6e11197a195d2242c2274609
realkalen <bettevans@yahoo.com>
2023-03-01 23:25:52 +0300

    post detail view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -15,6 +15,11 @@ class PostListView(ListView):
       template_name = 'blog/home.html'
       context_object_name = 'posts'
       ordering = '-date_posted'
+
+
+class PostDetailView(DetailView):
+      model = Post
+      
 
 def about(request):
         return render(request, 'blog/about.html', {'title': 'About'})
