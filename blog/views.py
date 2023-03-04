a641d09a1169d35d9a95b05e2b987aeb5d08c0fc
realkalen <bettevans@yahoo.com>
2023-03-04 16:30:41 +0300

    Post Update

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (
     ListView, 
     DetailView, 
-    CreateView
+    CreateView,
+    UpdateView
 )
 from .models import Post
 
@@ -27,6 +28,15 @@ class PostDetailView(DetailView):
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
+      model = Post
+      fields = ['title', 'content']
+
+      def form_valid(self, form):
+            form.instance.author = self.request.user
+            return super().form_valid(form)
+      
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
       model = Post
       fields = ['title', 'content']
 
