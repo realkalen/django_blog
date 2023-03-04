cbcd74fb1b3019bb30ce7874e46b0c9df9762f2d
realkalen <bettevans@yahoo.com>
2023-03-04 16:22:10 +0300

    LoginRequiredMixin

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (
     ListView, 
     DetailView, 
@@ -25,7 +26,7 @@ class PostDetailView(DetailView):
       model = Post
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
       model = Post
       fields = ['title', 'content']
 
