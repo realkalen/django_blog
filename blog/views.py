fee389587e4024fd2347d66de29c3978330d1c85
realkalen <bettevans@yahoo.com>
2023-03-04 16:57:40 +0300

    Delete View

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,8 @@ from django.views.generic import (
     ListView, 
     DetailView, 
     CreateView,
-    UpdateView
+    UpdateView,
+    DeleteView
 )
 from .models import Post
 
@@ -35,12 +36,6 @@ class PostCreateView(LoginRequiredMixin, CreateView):
             form.instance.author = self.request.user
             return super().form_valid(form)
       
-      def test_func(self):
-            post = self.get_object()
-            if self.request.user == post.author:
-                  return True
-            return False
-      
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
       model = Post
@@ -49,6 +44,22 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
       def form_valid(self, form):
             form.instance.author = self.request.user
             return super().form_valid(form)
+      
+      def test_func(self):
+            post = self.get_object()
+            if self.request.user == post.author:
+                  return True
+            return False
+      
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+      model = Post
+
+      def test_func(self):
+            post = self.get_object()
+            if self.request.user == post.author:
+                  return True
+            return False
 
 
 def about(request):
