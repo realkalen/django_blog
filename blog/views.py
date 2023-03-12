4096b1ef1938a4e856f20d93efb6bfd5637cc6f2
realkalen <bettevans@yahoo.com>
2023-03-12 22:51:06 +0300

    import django shortcuts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView, 
     DetailView, 
